b85f46e2d94d736b40d056029603672fe7262b2b
brunorpg <brunorpg@gmail.com>
2022-03-30 17:22:45 -0300

    Add pasta projeto final

---
+++ projeto-final/dioshopping-main/src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "familia",
+      fk_idcategorys: 1,
+      name_product: "Bandido",
+      price: 41.76,
+      image: "./images/produtos/bandido.jpg",
+      id_categorys: 1,
+      name_categorys: "Jogos para Família",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "familia",
+      fk_idcategorys: 1,
+      name_product: "Ice Cool",
+      price: 247.71,
+      image: "./images/produtos/icecool.jpg",
+      id_categorys: 1,
+      name_categorys: "Jogos para Família",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "competitivos",
+      fk_idcategorys: 1,
+      name_product: "Masmorra",
+      price: 464.91,
+      image: "./images/produtos/masmorra.jpg",
+      id_categorys: 2,
+      name_categorys: "Jogos Competitivos",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "competitivos",
+      fk_idcategorys: 2,
+      name_product: "Terraforming Mars",
+      price: 390.51,
+      image: "./images/produtos/terraforming.jpg",
+      id_categorys: 2,
+      name_categorys: "Jogos Competitivos",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "cooperativos",
+      fk_idcategorys: 2,
+      name_product: "Gloomhaven",
+      price: 1301.91,
+      image: "./images/produtos/gloomhaven.jpg",
+      id_categorys: 3,
+      name_categorys: "Jogos Cooperativos",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "cooperativos",
+      fk_idcategorys: 2,
+      name_product: "Mansions of Madness",
+      price: 743.91,
+      image: "./images/produtos/mansions.jpg",
+      id_categorys: 3,
+      name_categorys: "Jogos Cooperativos",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "cooperativos",
+      fk_idcategorys: 3,
+      name_product: "Pandemic: Reino de Cthulhu",
+      price: 328.99,
+      image: "./images/produtos/pandemicreino.jpg",
+      id_categorys: 3,
+      name_categorys: "Jogos Cooperativos",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "cooperativos",
+      fk_idcategorys: 3,
+      name_product: "Zombicide: Wulfsburg",
+      price: 325.41,
+      image: "./images/produtos/zombicidewulfburg.jpg",
+      id_categorys: 3,
+      name_categorys: "Jogos Cooperativos",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "dois",
+      fk_idcategorys: 4,
+      name_product: "Strifull",
+      price: 139.41,
+      image: "./images/produtos/strifull.jpg",
+      id_categorys: 4,
+      name_categorys: "Jogos para Dois",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "dois",
+      fk_idcategorys: 4,
+      name_product: "Santorini",
+      price: 232.41,
+      image: "./images/produtos/santorini.jpg",
+      id_categorys: 4,
+      name_categorys: "Jogos para Dois",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "infantis",
+      fk_idcategorys: 5,
+      name_product: "Sushi Go",
+      price: 102.21,
+      image: "./images/produtos/sushigo.jpg",
+      id_categorys: 5,
+      name_categorys: "Jogos infantis",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "infantis",
+      fk_idcategorys: 5,
+      name_product: "Catan Junior",
+      price: 260.31,
+      image: "./images/produtos/catanjr.jpg",
+      id_categorys: 5,
+      name_categorys: "Jogos infantis",
+      selected: false
+    },
+];
+
+export default productsList;
